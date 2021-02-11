8fa8ac5505fe5e8e83d899983228a3404a608845
flaviosenne123 <joaoflausino123@hotmail.com>
2021-02-11 19:07:58 -0300

    add annotation lombok in entities

--- src/main/java/com/joao/bookstoremanager/entity/Author.java
+++ src/main/java/com/joao/bookstoremanager/entity/Author.java
@@ -6,7 +6,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
